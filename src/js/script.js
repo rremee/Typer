9c4db17650c1b4ff2e66a6060f1e0dee022e8507
Anton Valkovskyi <u1remited@gmail.com>
2025-11-28 13:31:50 +0200

    spacebar added

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"],
 		["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'"],
 		["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
+        [" "]
 	];
 
 	const keyboard = document.querySelector(".game__keyboard");
@@ -20,6 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
 			keyElement.classList.add("keyboard__key");
 			keyElement.textContent = key;
 			keyElement.dataset.key = key;
+
+            if (keyElement.textContent === " ") keyElement.classList.add("keyboard__key--space");
 
 			rowElement.appendChild(keyElement);
 		});
