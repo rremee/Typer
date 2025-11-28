35de50b927007b4e79be8a6e9f667162e18d0a5f
Anton Valkovskyi <u1remited@gmail.com>
2025-11-28 09:11:14 +0200

    keyboard creation

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
+"use strict";
+
 import "/src/sass/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const keys = [
+		["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"],
+		["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'"],
+		["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
+	];
+
+	const keyboard = document.querySelector(".game__keyboard");
+
+	keys.forEach((row) => {
+		const rowElement = document.createElement("div");
+		rowElement.classList.add("keyboard__row");
+
+		row.forEach((key) => {
+			const keyElement = document.createElement("div");
+			keyElement.classList.add("keyboard__key");
+			keyElement.textContent = key;
+			keyElement.dataset.key = key;
+
+			rowElement.appendChild(keyElement);
+		});
+
+		keyboard.appendChild(rowElement);
+	});
+});
