e9ec00089b9b0fff2967396fce1dddf93eb28c66
Anton Valkovskyi <u1remited@gmail.com>
2025-12-17 16:32:49 +0200

    words.json (3k words); the logic of changing the main word

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"],
 		["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'"],
 		["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
-        [" "]
+		[" "],
 	];
 
 	const keyboard = document.querySelector(".game__keyboard");
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			keyElement.textContent = key;
 			keyElement.dataset.key = key;
 
-            if (keyElement.textContent === " ") keyElement.classList.add("keyboard__key--space");
+			if (keyElement.textContent === " ") keyElement.classList.add("keyboard__key--space");
 
 			rowElement.appendChild(keyElement);
 		});
@@ -30,37 +30,66 @@ document.addEventListener("DOMContentLoaded", () => {
 		keyboard.appendChild(rowElement);
 	});
 
-    document.addEventListener("keydown", (e) => {
-        const letter = e.key.toUpperCase();
-        const btn = document.querySelector(`[data-key="${letter}"]`);
+	document.addEventListener("keydown", (e) => {
+		const letter = e.key.toUpperCase();
+		const btn = document.querySelector(`[data-key="${letter}"]`);
 
-        if (btn) {
-            btn.classList.add("keyboard__key--active");
-        }
-    })
+		if (btn) {
+			btn.classList.add("keyboard__key--active");
+		}
+	});
 
-    document.addEventListener("keyup", (e) => {
-        const letter = e.key.toUpperCase();
-        const btn = document.querySelector(`[data-key="${letter}"]`);
+	document.addEventListener("keyup", (e) => {
+		const letter = e.key.toUpperCase();
+		const btn = document.querySelector(`[data-key="${letter}"]`);
 
-        if (btn) {
-            btn.classList.remove("keyboard__key--active");
-        }
-    })
+		if (btn) {
+			btn.classList.remove("keyboard__key--active");
+		}
+	});
 
-    const timer = document.querySelector("#timer");
-    let time = 15;
+	const timer = document.querySelector("#timer");
+	let time = 15;
 
-    // timer.textContent = time;
+	// timer.textContent = time;
 
-    const timerInterval = setInterval(() => {
-        if (time > 0) {
-            time -= 0.01;
-            timer.textContent = time.toFixed(2) + " s";
-        } else {
-            timer.textContent = "00.00 s";
-            clearInterval(timerInterval);
-        }
-    }, 10);
+	const timerInterval = setInterval(() => {
+		if (time > 0) {
+			time -= 0.01;
+			timer.textContent = time.toFixed(2) + " s";
+		} else {
+			timer.textContent = "00.00 s";
+			clearInterval(timerInterval);
+		}
+	}, 10);
 
+	let words = [];
+	let currentWord = "";
+
+	const mainWord = document.querySelector("#word");
+	const wordInput = document.querySelector("#word-input");
+
+	function getRandomWord() {
+		return words[Math.round(Math.random() * words.length)];
+	}
+
+	function showNewWord() {
+		currentWord = getRandomWord();
+		mainWord.textContent = currentWord;
+		wordInput.value = "";
+		wordInput.focus();
+	}
+
+	fetch("/src/data/words.json")
+		.then((res) => res.json())
+		.then((data) => {
+			words = data.words;
+			showNewWord();
+		});
+
+	wordInput.addEventListener("input", () => {
+		if (mainWord.textContent.toLowerCase() === wordInput.value.trim().toLowerCase()) {
+			showNewWord();
+		}
+	});
 });
